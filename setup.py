62ed5b21d4b21fe0c98f4e5d6a0b876cba16def5
chrdt <christoph.dietel@yahoo.de>
2017-12-12 14:15:05 +0100

    Adds setuptools to manage the project

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, command
+
+NAME = 'src'
+DESCRIPTION = 'Project for a lecture about Algorithm Engineering'
+
+REQUIRED = [
+    'requests', 'scapy', 'pygal',
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+	long_description = '\n' + f.read()
+
+setup(
+	name=NAME,
+	description=DESCRIPTION,
+	long_description=long_description,
+	packages=find_packages(),
+	install_requires=REQUIRED,
+	include_package_data=True,
+)
