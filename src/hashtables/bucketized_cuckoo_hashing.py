41351e1dafb4d5a22ec7319497b9e8cf1741cb87
chrdt <christoph.dietel@yahoo.de>
2017-12-14 19:26:14 +0100

    Adds bucketized cuckoo hashing and optimizes cuckoo hashing

--- src/hashtables/bucketized_cuckoo_hashing.py
+++ src/hashtables/bucketized_cuckoo_hashing.py
@@ -1,21 +1,131 @@
 #!/usr/bin/env python2
 from __future__ import print_function
+from random import randint
+from math import log
+import numpy
+from itertools import chain
 
 from hashtable import HashTable
+from src.util.hash_functions import universal_hash
+from src.util.timer import timer
+
+
+class BucketTable(object):
+    """
+    Used for the hash tables
+    Each table consists of size buckets
+    Each bucket is a list of length 9
+    bucket[0] next usable index or 5
+    bucket[1..4] fields for keys
+    bucket[5..8] fields for values
+    key bucket[i] and value bucket[i+4] are a pair
+    """
+    def __init__(self, size):
+        self.size = size
+        self._table = size * [[]]
+        self._hf = universal_hash(size)
+
+    def pprint(self):
+        print(self._table)
+
+    def insert(self, key, value):
+        addr = self._hf(key)
+
+        try:
+            bucket = self._table[addr]
+            bucket_index = bucket[0]
+        except IndexError:
+            bucket = self._table[addr] = [2, key, None, None, None, value, None, None, None]
+            return (0, 0)
+        
+        if bucket_index == 5:
+            evicted_key = bucket[1]
+            evicted_val = bucket[5]
+            self._table[addr][1] = key
+            self._table[addr][5] = value
+            return (evicted_key, evicted_val)
+        else:
+            self._table[addr][bucket_index] = key
+            self._table[addr][bucket_index + 4] = value
+            self._table[addr][0] += 1
+            return (0, 0)
+
+    def items(self):
+        return (
+                (key, bucket[index+4]) 
+                    for bucket in self._table
+                        for index, key in enumerate(bucket[1:5])
+                            if key
+               )
 
 
 class BucketizedCuckooHash(HashTable):
-    def pprint(self):
-        pass
+    """
+    A variant of Cuckoo Hashing as explained in http://crpit.com/confpapers/CRPITV91Askitis.pdf
+    Each slot is a bucket that contains up to four entries
+    """
+    def __init__(self, size):
+        self.size = size
+        self._elementcount = 0
+        self._maxloop = int(log(size))
+        self._table1 = BucketTable(size)
+        self._table2 = BucketTable(size)
+        self._stash = numpy.full((8192, 2), 0, dtype=numpy.int)
+        self._stashf = lambda x : x % 8192
 
-    def tosequence(self):
-        pass
+    def insert(self, key, value): # 19,737 s
+        for _ in xrange(self._maxloop):
+            key, value = self._table1.insert(key, value) # 14 s
+            if key == 0:
+                self.ckeck_for_rehash_and_resize() # 12 s
+                return
 
-    def insert(self, key, value):
-        pass
+            key, value = self._table2.insert(key, value)
+            if key == 0:
+                self.ckeck_for_rehash_and_resize()
+                return
 
-    def delete(self, key):
-        pass
+        print("\tStashing key {} and value {}".format(key, value))
+        stash_pos = self._stashf(key)
+        self._stash[stash_pos][0] = key
+        self._stash[stash_pos][1] = value
+
+    def rehash(self):
+        entries1 = self._table1.items()
+        entries2 = self._table2.items()
+        self._elementcount = 0
+        self._maxloop = int(log(self.size))
+        self._table1 = BucketTable(self.size)
+        self._table2 = BucketTable(self.size)
+
+        for key, value in chain(entries1, entries2): 
+            self.insert(key, value)
+
+    def ckeck_for_rehash_and_resize(self):
+        self._elementcount += 1
+        if self._elementcount > 5*self.size:
+            print("\tRehashing and Resizing with size={} and elemencount={}".format(self.size, self._elementcount))
+            self.size *= 2
+            self.rehash()
 
     def search(self, key):
         pass
+
+    def contains(self, key):
+        pass
+        
+    def tosequence(self):
+        pass
+
+    def pprint(self):
+        print("-------------- TABLE1")
+        # self._table1.pprint()
+
+        print("-------------- TABLE2")
+        # self._table2.pprint()
+
+if __name__ == '__main__':
+    ht = BucketizedCuckooHash(128)
+    for i in range(1, 1000000):
+        ht.insert(i,i)
+    ht.pprint()
--- src/hashtables/cuckoo_hashing.py
+++ src/hashtables/cuckoo_hashing.py
@@ -2,28 +2,36 @@
 from __future__ import print_function
 from random import randint
 from math import log
+import numpy
 
 from hashtable import HashTable
 from src.util.hash_functions import universal_hash
+from src.util.timer import timer
 
 
 class CuckooHash(HashTable):
+    """
+    Pagh and Rodler's Cuckoo Hashing
+    Uses a stash for keys that cause irresolvable collisions
+    """
     def __init__(self, size):
         self.size = size
         self._elementcount = 0
         self._maxloop = int(log(size))
-        self._table2 = [None]*size
-        self._table1 = [None]*size
-        self._hf2 = universal_hash(size)
+        self._table1 = numpy.full(size, 0, dtype=self.entry)
+        self._table2 = numpy.full(size, 0, dtype=self.entry)
         self._hf1 = universal_hash(size)
+        self._hf2 = universal_hash(size)
+        self._stash = numpy.full(8192, 0, dtype=self.entry)
+        self._stashf = lambda x : x % 8192
 
     def reset(self):
         self._elementcount = 0
         self._maxloop = int(log(self.size))
-        self._table2 = [None]*self.size
-        self._table1 = [None]*self.size
-        self._hf2 = universal_hash(self.size)
+        self._table1 = numpy.full(self.size, 0, dtype=self.entry)
+        self._table2 = numpy.full(self.size, 0, dtype=self.entry)
         self._hf1 = universal_hash(self.size)
+        self._hf2 = universal_hash(self.size)
 
     def insert(self, key, value):
         addr1 = self._hf1(key)
@@ -63,10 +71,16 @@ class CuckooHash(HashTable):
                 self.ckeck_for_rehash_and_resize()
                 return
 
-        print("\tRehashing after reaching maximal loops")
+        print("\tStashing key {} and value {}".format(key, value))
+        stash_pos = self._stashf(key)
+        self._stash[stash_pos] = self.entry(key, value)
+
+        """
+        In the original version a rehash would occur at this point:
         self.rehash()
         self.insert(key, value)
-
+        """
+    
     def rehash(self):
         ht1 = [entry for entry in self._table1[:] if entry]
         ht2 = [entry for entry in self._table2[:] if entry]
@@ -77,7 +91,7 @@ class CuckooHash(HashTable):
 
     def ckeck_for_rehash_and_resize(self):
         self._elementcount += 1
-        if self._elementcount > self.size // 2:
+        if self._elementcount >= self.size:
             print("\tRehashing and Resizing with size={} and elemencount={}".format(self.size, self._elementcount))
             self.size *= 2
             self.rehash()
@@ -125,7 +139,8 @@ class CuckooHash(HashTable):
 
 if __name__ == '__main__':
     #ht = dict()
-    ht = CuckooHash(8)
+    ht = CuckooHash(64)
     for i in range(1000000):
         ht.insert(i,i)
+
     # ht.pprint()
