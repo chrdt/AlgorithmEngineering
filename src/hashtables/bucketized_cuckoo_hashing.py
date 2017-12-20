0bb58d0ea0c6f5a2ff4821708de0d6697d9e228e
chrdt <christoph.dietel@yahoo.de>
2017-12-20 19:25:51 +0100

    Removes numpy and implements contains

--- src/hashtables/bucketized_cuckoo_hashing.py
+++ src/hashtables/bucketized_cuckoo_hashing.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python2
 from __future__ import print_function
-from random import randint
 from math import log
-import numpy
 from itertools import chain
 
 from hashtable import HashTable
 from src.util.hash_functions import universal_hash
-from src.util.timer import timer
 
 
 class BucketTable(object):
@@ -28,6 +25,32 @@ class BucketTable(object):
     def pprint(self):
         print(self._table)
 
+    def contains(self, key):
+        addr = self._hf(key)
+
+        try:
+            bucket = self._table[addr]
+            bucket_index = bucket[0]
+        except IndexError:
+            return False
+
+        # return len([curr for curr in bucket[1:bucket_index] if curr == key])
+        return any((curr == key for curr in bucket[1:bucket_index]))
+
+    def search(self, key):
+        addr = self._hf(key)
+
+        try:
+            bucket = self._table[addr]
+            bucket_index = bucket[0]
+        except IndexError:
+            return 0
+
+        for index, curr in enumerate(bucket[1:bucket_index], start=1):
+            if curr == key:
+                return bucket[index]
+        return 0
+
     def insert(self, key, value):
         addr = self._hf(key)
 
@@ -36,27 +59,27 @@ class BucketTable(object):
             bucket_index = bucket[0]
         except IndexError:
             bucket = self._table[addr] = [2, key, None, None, None, value, None, None, None]
-            return (0, 0)
-        
+            return 0, 0
+
         if bucket_index == 5:
             evicted_key = bucket[1]
             evicted_val = bucket[5]
             self._table[addr][1] = key
             self._table[addr][5] = value
-            return (evicted_key, evicted_val)
+            return evicted_key, evicted_val
         else:
             self._table[addr][bucket_index] = key
             self._table[addr][bucket_index + 4] = value
             self._table[addr][0] += 1
-            return (0, 0)
+            return 0, 0
 
     def items(self):
         return (
-                (key, bucket[index+4]) 
-                    for bucket in self._table
-                        for index, key in enumerate(bucket[1:5])
-                            if key
-               )
+            (key, bucket[index+4])
+            for bucket in self._table
+                for index, key in enumerate(bucket[1:5], start=1)
+                    if key
+        )
 
 
 class BucketizedCuckooHash(HashTable):
@@ -70,14 +93,14 @@ class BucketizedCuckooHash(HashTable):
         self._maxloop = int(log(size))
         self._table1 = BucketTable(size)
         self._table2 = BucketTable(size)
-        self._stash = numpy.full((8192, 2), 0, dtype=numpy.int)
-        self._stashf = lambda x : x % 8192
+        self._stash = dict()
+        self._stashf = lambda x: x % 8192
 
-    def insert(self, key, value): # 19,737 s
+    def insert(self, key, value):
         for _ in xrange(self._maxloop):
-            key, value = self._table1.insert(key, value) # 14 s
+            key, value = self._table1.insert(key, value)
             if key == 0:
-                self.ckeck_for_rehash_and_resize() # 12 s
+                self.ckeck_for_rehash_and_resize()
                 return
 
             key, value = self._table2.insert(key, value)
@@ -86,9 +109,8 @@ class BucketizedCuckooHash(HashTable):
                 return
 
         print("\tStashing key {} and value {}".format(key, value))
-        stash_pos = self._stashf(key)
-        self._stash[stash_pos][0] = key
-        self._stash[stash_pos][1] = value
+        self.pprint()
+        self._stash[key] = value
 
     def rehash(self):
         entries1 = self._table1.items()
@@ -98,34 +120,43 @@ class BucketizedCuckooHash(HashTable):
         self._table1 = BucketTable(self.size)
         self._table2 = BucketTable(self.size)
 
-        for key, value in chain(entries1, entries2): 
+        for key, value in chain(entries1, entries2):
             self.insert(key, value)
 
     def ckeck_for_rehash_and_resize(self):
         self._elementcount += 1
-        if self._elementcount > 5*self.size:
+        if self._elementcount > 4*self.size:
             print("\tRehashing and Resizing with size={} and elemencount={}".format(self.size, self._elementcount))
             self.size *= 2
             self.rehash()
 
     def search(self, key):
-        pass
+        return self._table1.search(key) or self._table2.search(key) or self._stash[key]
 
     def contains(self, key):
-        pass
-        
+        return self._table1.contains(key) or self._table2.contains(key) or key in self._stash
+
     def tosequence(self):
-        pass
+        entries1 = self._table1.items()
+        entries2 = self._table2.items()
+        entries3 = ((key, self._stash[key]) for key in self._stash)
+
+        return "\n".join(str(key) for key, _ in chain(entries1, entries2, entries3))
 
     def pprint(self):
-        print("-------------- TABLE1")
-        # self._table1.pprint()
+        print("-------------- TABLE1:")
+        self._table1.pprint()
 
-        print("-------------- TABLE2")
-        # self._table2.pprint()
+        print("-------------- TABLE2:")
+        self._table2.pprint()
+
+        print("-------------- STASH:")
+        for key, val in self._stash.items():
+            print("{}: {}".format(key, val))
 
 if __name__ == '__main__':
-    ht = BucketizedCuckooHash(128)
+    ht = BucketizedCuckooHash(64)
     for i in range(1, 1000000):
-        ht.insert(i,i)
-    ht.pprint()
+        ht.insert(i, i)
+        ht.contains(i)
+    x = ht.tosequence()
